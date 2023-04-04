e91eacdb50bd4c9ff8a9f20ca7f37ba965b0cc98
Tetiana2023 <drofaj18@gmail.com>
2023-04-04 22:20:29 +0300

    клік відбувається по лішці(li перенесла в Link)

--- src/components/MoviesList/MoviesList.jsx
+++ src/components/MoviesList/MoviesList.jsx
@@ -2,21 +2,21 @@ import { Link, useLocation } from 'react-router-dom';
 import css from './MoviesList.module.css';
 import PropTypes from 'prop-types';
 
- const MoviesList = ({ movies }) => {
+const MoviesList = ({ movies }) => {
   const location = useLocation();
   return (
     <>
       <ul className={css.moviesList}>
         {movies.map(({ id, title, name }) => (
+          <Link
+            className={css.movieLink}
+            to={`/movies/${id}`}
+            state={{ from: location }}
+          >
           <li key={id} className={css.item}>
-            <Link
-              className={css.movieLink}
-              to={`/movies/${id}`}
-              state={{ from: location }}
-            >
               {title || name}
-            </Link>
-          </li>
+            </li>
+          </Link>
         ))}
       </ul>
     </>
@@ -25,11 +25,11 @@ import PropTypes from 'prop-types';
 export default MoviesList;
 
 MoviesList.propTypes = {
-  movies:PropTypes.arrayOf(
+  movies: PropTypes.arrayOf(
     PropTypes.shape({
-      id:PropTypes.number,
-      title:PropTypes.string,
-      name:PropTypes.string,
+      id: PropTypes.number,
+      title: PropTypes.string,
+      name: PropTypes.string,
     })
-  )
-}
+  ),
+};
